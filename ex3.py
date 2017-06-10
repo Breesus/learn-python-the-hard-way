262580d926ed70874c590b1f6fffbf0a5a20fd44
Igor <igor.alves.silva@usp.br>
2017-06-10 20:25:52 -0300

    ex3

--- ex3.py
+++ ex3.py
@@ -1,50 +1,21 @@
-print "I will now count my chikens:"
+print ("I will now count my chikens:")
 
-print
-print "Hens", 25 + 30 / 6
+print()
+print ("Hens", 25 + 30 / 6)
 # 1.  30 / 6 = 5
 # 2.  25 + 5 = 30
 
-print "roosters", 100 - 25 * 3 % 4
+print ("roosters", 100 - 25 * 3 % 4)
 # 1.  25 * 3 = 75
 # 2.  75 % 4 = 3    because 75/4 = 18 r 3
 # 3.  100 - 3 = 97
 
-print "Now I will count the eggs:"
+print ("Now I will count the eggs:")
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 # 1.  4 % 0 = 0   because 4/2 has no remainder
 # 2.  1.0 / 4 = 0.25    need .0 to make floating point
 # 3.  Then add and subtract left to right
 #     3 + 2 + 1 - 5 + 0 - 0.25 + 6 = 6.75
 
-print
-print "Is it true that 3 + 2 < 5 - 7?"
-# Is 5 < -2?
 
-print 3 + 2 < 5 - 7
-
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
-
-print "Oh, thats why its Fales."
-
-print "How about some more."
-
-print "Is it greater?", 5 > -2
-
-print "Is it greater or equal?", 5 >= -2
-
-print "Is it less or equal?", 5 <= -2
-
-print
-# intergers vs floating point numbers
-
-print
-print "These show math with intergers vs. floating point numbers"
-
-print "5/4 =", 5/4, "The .25 got truncated"
-
-print "5.0/4 =", 5.0/4
-print "5/4.0 =", 5/4.0
-print "5.0/4.0 =", 5.0/4.0
