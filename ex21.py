939f9f4dc4b54dc4cf31ec2d0768297550bbe23a
capone <cap12008@live.com>
2017-06-10 23:42:06 +0100

    py the hard way

--- ex21.py
+++ ex21.py
@@ -13,6 +13,10 @@ def multiply(a, b):
 def divide(a, b):
     print "DIVIDING %d / %d" % (a, b)
     return a / b
+# creating math functions
+# return holds value of sum which can
+# be assigned to a variable
+# don't need print line
 
 
 print "Let's do some math with just functions!"
@@ -21,13 +25,15 @@ age = add(30, 5)
 height = subtract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
+# calling the functions to assign values to variables
 
 print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
-
+# calling variables with function values assigned
 
 # a puzzle
 print "Here is a puzzle."
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
-
-print "That becomes: ", what, "Can you do it by hand" 
+# Order of operations
+# start with first parentheses first and go back wards
+print "That becomes: ", what, "Can you do it by hand"
